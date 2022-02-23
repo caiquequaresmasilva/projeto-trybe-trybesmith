0ea23491df25caeaeecdfee0c7bbdab523766d7c
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-02-23 12:00:04 -0300

    Criado metodo create no Controller products

--- src/controllers/Product.ts
+++ src/controllers/Product.ts
@@ -1,1 +1,22 @@
-export default {};
+import { NextFunction, Request, Response } from 'express';
+import Product from '../services/Products';
+import StatusCode from '../enums/StatusCode';
+import { IProduct, IResponseProd } from '../interfaces/Product';
+
+const validateProd = (req:Request, _res:Response, next:NextFunction) => {
+  const prod: IProduct = req.body;
+  const validation = Product.validateProd(prod);
+  if (validation.error) return next(validation.error);
+  next();
+};
+
+const create = async (req:Request, res:Response) => {
+  const user: IProduct = req.body;
+  const newProd: IResponseProd = await Product.create(user);
+  res.status(StatusCode.CREATED).json(newProd);
+};
+
+export default {
+  validateProd,
+  create,
+};
--- src/services/Products.ts
+++ src/services/Products.ts
@@ -1,3 +1,19 @@
 import Product from '../models/Product';
-import StatusCode from '../enums/StatusCode';
+// import StatusCode from '../enums/StatusCode';
 import productSchema from '../schemas/productSchema';
+import { INewProduct, IProduct, IResponseProd } from '../interfaces/Product';
+
+const validateProd = (product: IProduct) => {
+  const validation = productSchema.validate(product);
+  return validation;
+};
+
+const create = async (product: IProduct) : Promise<IResponseProd> => {
+  const newProd: INewProduct = await Product.create(product);
+  return { item: newProd };
+};
+
+export default {
+  create,
+  validateProd,
+};
